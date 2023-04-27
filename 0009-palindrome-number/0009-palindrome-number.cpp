05be4ba5e43a0bac9a7226aa40bbb2ad8a6b6f74
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-04-27 15:57:25 +0530

    Time: 28 ms (13.61%), Space: 5.9 MB (85.54%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       long int rev=0;
+        int n=x;
+        if(x<0)
+        {
+            return false;
+        }
+        while(x!=0)
+        {
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+        }
+        if(n==rev)
+        {
+           return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+    }
+};
