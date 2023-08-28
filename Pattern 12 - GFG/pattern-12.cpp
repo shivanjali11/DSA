49d0d3c94ff449a3bccdb52a869c1d2dfd9d06a2
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-08-28 15:41:25 +0530

    Added solution - LeetHub

--- Pattern 12 - GFG/pattern-12.cpp
+++ Pattern 12 - GFG/pattern-12.cpp
@@ -7,28 +7,25 @@ using namespace std;
 class Solution {
   public:
     void printTriangle(int n) {
-           for (int i = 1; i <= n; i++) {
-        // Print the left side numbers
-        for (int j = 1; j <= i; j++) {
-            cout << j << " ";
-        }
-        
-        // Print the spaces in the middle
-        int spaces = 2 * (n - i);
-        for (int j = 1; j <= spaces; j++) {
-            cout << "  "; // Two spaces for formatting
-        }
-        
-        // Print the right side numbers
-        for (int j = i; j >= 1; j--) {
-            cout << j;
-            if (j > 1) {
-                cout << " ";
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                cout<<j<<" ";
             }
+            for(int j=0;j<2*(n-i);j++)
+            {
+                cout<<" ";
+            }
+            for(int j=i;j>=1;j--)
+            {
+                cout<<j<<" ";
+            } 
+            cout<<endl;
+            
+            
+            
         }
-        
-        cout << endl;
-    }
         // code here
     }
 };
