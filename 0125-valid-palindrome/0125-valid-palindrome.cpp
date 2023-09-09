806c234805a90984338f75e98c955c198a3b92a2
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-09-09 22:04:41 +0530

    Time: 4 ms (76.36%), Space: 7.2 MB (98.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            while (left < right && !isalnum(s[left])) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right)
+        {
+//             to skip the non alphanumeric characters and check also for left<right and increment the left until it encounter alphanumeric characters
+            while(left<right&& !isalnum(s[left]))
+            {
                 left++;
             }
-            while (left < right && !isalnum(s[right])) {
+//             same concept
+             while(left<right&& !isalnum(s[right]))
+            {
                 right--;
             }
-
-            if (tolower(s[left]) != tolower(s[right])) {
+            if(tolower(s[left])!= tolower(s[right]))
                 return false;
-            }
-
             left++;
             right--;
         }
-
         return true;
+        
     }
-
-    
 };
