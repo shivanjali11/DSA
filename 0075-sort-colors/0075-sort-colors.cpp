0c8bea9b5aa3edbc62708fa3431b88306b495e57
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-09-28 20:46:14 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (15.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -14,33 +14,19 @@ public:
                     if(nums[i]==2)
                             c++;
             }
-            for(int i=0;i<n;i++)
+            
+            for(int i=0;i<a;i++)
             {
-                    if(i<a&&i>=0)
-                    {
-                            nums[i]=0;
-                    }
-                    if(i<a+b&&i>=a)
-                    {
-                            nums[i]=1;
-                    }
-                    if(i<a+b+c&&i>=a+b)
-                    {
-                            nums[i]=2;
-                    }
+                    nums[i]=0;
             }
-            // for(int i=0;i<a;i++)
-            // {
-            //         nums[i]=0;
-            // }
-            //  for(int i=a;i<a+b;i++)
-            // {
-            //         nums[i]=1;
-            // }
-            //   for(int i=b;i<a+b+c;i++)
-            // {
-            //         nums[i]=2;
-            // }
+             for(int i=a;i<a+b;i++)
+            {
+                    nums[i]=1;
+            }
+              for(int i=a+b;i<a+b+c;i++)
+            {
+                    nums[i]=2;
+            }
         
     }
 };
