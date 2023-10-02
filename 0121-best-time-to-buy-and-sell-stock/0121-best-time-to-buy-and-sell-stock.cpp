cd24979aa323bcef0e63a1a0fac8091316f3594e
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-02 21:55:41 +0530

    Time: 106 ms (15.32%), Space: 93.8 MB (8.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& a) {
-             int n=a.size();
+      
+    // Write your code here.
+    int n=a.size();
     int low=0;
     int high=1;
-    int profit;
+    int profit=0;
     int maxi=0;
    while(high<n)
    {
@@ -13,14 +15,14 @@ public:
            profit=a[high]-a[low];
            maxi=max(maxi,profit);
            
-           
 
        }
        else
        low=high;
-           high++;
+       high++;
    }
    return maxi;
-        
-    }
+}
+
+    
 };
