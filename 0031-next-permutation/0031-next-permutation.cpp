afd77e2951da234b1c3f66e004a98efba439bf14
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-04 22:17:33 +0530

    Time: 4 ms (47.26%), Space: 12.4 MB (13.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,33 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-//             this vector stores the permutation of a given array
-            int n=nums.size();
-//             int fact=1;
-//             for(int i=n;i>0;i--)
-//             {
-//                   fact=fact*(n);  
-//             }
-//             int times=fact;
-//             vector<vectot<int>>v;
-//             for(int i=1;i<=times;i++)
-//             {
-                    
-//             }
-//             BETTER APPROACH USING C++ FUNCTION
-            next_permutation(nums.begin(),nums.end());
-            cout<<"[";
-            for(int i=0;i<n;i++)
-            {
-                    cout<<nums[i];
-            }
-            cout<<"]"<<endl;
+    void nextPermutation(vector<int>& a) {
+             int n=a.size();
+    int ind=-1;
+    for(int i=n-2;i>=0;i--)
+    {
+        if(a[i+1]>a[i])
+        {
+            ind=i;
+            break;
+        }
+    }
+    if(ind==-1)
+    {
+        reverse(a.begin(),a.end());
+            return;
+        
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        if(a[ind]<a[i])
+        {
+            swap(a[ind],a[i]);
+            break;
+        }
+    }
+    sort(a.begin()+ind+1,a.end());
+            return ;
+    
         
     }
 };
