49785372ec91be89dbd0c1653365f738375e5b2b
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-17 14:20:18 +0530

    Time: 17 ms (9.45%), Space: 13.7 MB (41.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,57 +1,31 @@
 class Solution {
 public:
-        void row(vector<vector<int>>&a,int i,int m)
-        {
-                for(int j=0;j<m;j++)
-                {
-                        if(a[i][j]!=0)
-                        {
-                                a[i][j]='l';
-}
-                }
-}
-        void column(vector<vector<int>>&a,int j,int n)
-        {
-                for(int i=0;i<n;i++)
-                {
-                        if(a[i][j]!=0)
-                        {
-                                a[i][j]='l';
-}
-                }
-}
-    void setZeroes(vector<vector<int>>& matrix) {
-            int n=matrix.size();
-            int m=(n>0)?matrix[0].size():0;
-            int count=0;
+    void setZeroes(vector<vector<int>>& a) {
+            int n=a.size();
+            int m=a[0].size();
+            vector<int>row(n,0);
+            vector<int>column(m,0);
             for(int i=0;i<n;i++)
             {
                     for(int j=0;j<m;j++)
                     {
-                            if(matrix[i][j]==0)
+                            if(a[i][j]==0)
                             {
-                                    row(matrix,i,m);
-                                    column(matrix,j,n);
+                                    row[i]=1;
+                                    column[j]=1;
                             }
-                            else
-                            count++;
                     }
             }
-            if(count==m*n)
-            {
-                    return ;
-            }
             for(int i=0;i<n;i++)
             {
                     for(int j=0;j<m;j++)
                     {
-                            if(matrix[i][j]=='l')
+                            if(row[i]==1||column[j]==1)
                             {
-                                    matrix[i][j]=0;
-                             }
-                      }
+                                    a[i][j]=0;
+                            }
+                    }
             }
-            
             
         
     }
