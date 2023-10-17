d248ddfe07ff55f6e7342f4794dc1548f5cc21c2
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-17 09:54:27 +0530

    Time: 10 ms (69.99%), Space: 13.8 MB (41.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
+        void row(vector<vector<int>>&a,int i,int m)
+        {
+                for(int j=0;j<m;j++)
+                {
+                        if(a[i][j]!=0)
+                        {
+                                a[i][j]='l';
+}
+                }
+}
+        void column(vector<vector<int>>&a,int j,int n)
+        {
+                for(int i=0;i<n;i++)
+                {
+                        if(a[i][j]!=0)
+                        {
+                                a[i][j]='l';
+}
+                }
+}
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = (n > 0) ? matrix[0].size() : 0;
-
-        // Use the first row and first column to mark which rows and columns should be zeroed
-        bool zeroFirstRow = false;
-        bool zeroFirstCol = false;
-
-        // Check if the first row and first column need to be zeroed
-        for (int j = 0; j < m; j++) {
-            if (matrix[0][j] == 0) {
-                zeroFirstRow = true;
-                break;
+            int n=matrix.size();
+            int m=(n>0)?matrix[0].size():0;
+            int count=0;
+            for(int i=0;i<n;i++)
+            {
+                    for(int j=0;j<m;j++)
+                    {
+                            if(matrix[i][j]==0)
+                            {
+                                    row(matrix,i,m);
+                                    column(matrix,j,n);
+                            }
+                            else
+                            count++;
+                    }
             }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                zeroFirstCol = true;
-                break;
+            if(count==m*n)
+            {
+                    return ;
             }
-        }
-
-        // Mark rows and columns using the first row and first column
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
+            for(int i=0;i<n;i++)
+            {
+                    for(int j=0;j<m;j++)
+                    {
+                            if(matrix[i][j]=='l')
+                            {
+                                    matrix[i][j]=0;
+                             }
+                      }
             }
-        }
-
-        // Zero out rows and columns
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Zero out the first row and first column if needed
-        if (zeroFirstRow) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (zeroFirstCol) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
-            }
-        }
+            
+            
+        
     }
 };
