63d141c43020c49040bb3704055fa3a578ad8b3c
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-19 03:10:52 +0530

    Time: 16 ms (7.31%), Space: 16.4 MB (12.24%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+           // USING HASHMAP
             int n=nums.size();
             vector<int>ans;
             map<int,int>m;
             for(int i=0;i<n;i++)
             {
                    m[nums[i]]++;
-            }
-            for(auto it:m)
-            {
-                
-                    if(it.second>(n/3))
+
+                    if(m[nums[i]]==(n/3)+1)
                     {
-                            ans.push_back(it.first);
-                            
+                            ans.push_back(nums[i]);
                     }
+                    if(ans.size()==2)
+                            break;
             }
+           
             return ans;
         
     }
