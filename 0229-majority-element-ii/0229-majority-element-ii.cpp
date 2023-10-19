456b975832f56bcc8c7137c08f93ac795fe4a8cd
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-19 18:50:22 +0530

    Time: 16 ms (7.31%), Space: 16.4 MB (29.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,56 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-           // USING HASHMAP
+            int count1=0;
+            int count2=0;
+            int ele1;
+            int ele2;
             int n=nums.size();
             vector<int>ans;
-            map<int,int>m;
             for(int i=0;i<n;i++)
             {
-                   m[nums[i]]++;
-
-                    if(m[nums[i]]==(n/3)+1)
+                   if(count1==0&&ele2!=nums[i])
+                   {
+                           count1=1;
+                           ele1=nums[i];
+                   }
+                    else if(count2==0&&ele1!=nums[i])
                     {
-                            ans.push_back(nums[i]);
+                            count2=1;
+                            ele2=nums[i];
                     }
-                    if(ans.size()==2)
-                            break;
+                    else if(ele1==nums[i])
+                    {
+                            count1++;
+                    }
+                    else if(ele2==nums[i])
+                    {
+                            count2++;
+                    }
+                    else
+                    {
+                            count1--;
+                            count2--;
+                    }
             }
-           
+            int count=0;
+            int countt=0;
+            
+           for(int i=0;i<n;i++)
+           {
+                   if(ele1==nums[i])
+                   {
+                           count++;
+                   }
+                   if(ele2==nums[i])
+                   {
+                           countt++;
+                   }
+           }
+            if(count>(n/3))
+                    ans.push_back(ele1);
+            if(countt>(n/3))
+                    ans.push_back(ele2);
             return ans;
         
     }
