5b54c27762713b5cf288d2cf39d6500374812f72
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-10-24 16:01:29 +0530

    Time: 37 ms (21.24%), Space: 19.3 MB (69.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-          int n=arr.size();
-    vector<vector<int>>ans;
-    sort(arr.begin(),arr.end());
-    for(int i=0;i<n;i++)
-    {
-        int start=arr[i][0];
-        int end=arr[i][1];
-        if(!ans.empty()&&end<=ans.back()[1])
-        {
-            continue;
-        }
-        for(int j=i+1;j<n;j++)
-        {
-           if(arr[j][0]<=end&&start<=arr[j][0])
-           {
-               if(end<=arr[j][1])
-               {
-                   end=arr[j][1];
-               }
-               
-           }
-           else
-           {
-               break;
-           }
-        }
-        ans.push_back({start,end});
-    }
-    return ans;
+            int n=arr.size();
+            vector<vector<int>>ans;
+            sort(arr.begin(),arr.end());
+            for(int i=0;i<n;i++)
+            {
+//                     interval only creates when either ans would be empty or if not overlapping condition occurs
+                    if(ans.empty()||ans.back()[1]<arr[i][0])
+                    {
+//                      push the new pair in the vector
+                         ans.push_back(arr[i]);   
+                    }
+                    else
+                    {
+                           if(ans.back()[1]<arr[i][1])
+                           {
+                                   ans.back()[1]=arr[i][1];
+                           }
+                    }
+            }
+            return ans;
     }
 };
