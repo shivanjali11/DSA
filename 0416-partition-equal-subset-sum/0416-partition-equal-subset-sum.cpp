dafd16271f4d15837eddfb4d733889bd11382fcc
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-11-04 19:46:06 +0530

    Time: 145 ms (81.72%), Space: 12.2 MB (79.21%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+        bool subsequence_sum(vector<int> nums, int sum) {
+    int n = nums.size();
+    bool t[n + 1][sum + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= sum; j++) {
+            if (i == 0) {
+                t[i][j] = false;
+            }
+            if (j == 0) {
+                t[i][j] = true;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (nums[i - 1] <= j) {
+                t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
+            } else {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+
+    return t[n][sum];
+}
+    bool canPartition(vector<int>& nums) {
+            int n=nums.size();
+            int sum=0;
+            for(int i=0;i<n;i++)
+            {
+                  sum+=nums[i];  
+            }
+            if(sum%2!=0)
+                    return false;
+            else
+                    return subsequence_sum(nums,sum/2);
+        
+    }
+};
