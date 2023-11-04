72a2d90cf6fdfc6a536bab61ddbfd8c0878749e1
Shivanjali Kumari <111866443+shivanjali11@users.noreply.github.com>
2023-11-04 19:49:08 +0530

    Time: 143 ms (82.15%), Space: 12.1 MB (79.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-        bool subsequence_sum(vector<int> nums, int sum) {
-    int n = nums.size();
-    bool t[n + 1][sum + 1];
-
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= sum; j++) {
-            if (i == 0) {
-                t[i][j] = false;
-            }
-            if (j == 0) {
-                t[i][j] = true;
-            }
-        }
+    bool subsequence_sum(vector<int> nums,int sum)
+    {
+            int n=nums.size();
+            bool t[n+1][sum+1];
+            for(int i=0;i<n+1;i++)
+            {
+                    for(int j=0;j<sum+1;j++)
+                    {
+                            if(i==0)
+                            t[i][j]=false;
+                            if(j==0)
+                            t[i][j]=true;
+                                   
+                    } }
+                    for(int i=1;i<=n;i++)
+                    {
+                            for(int j=1;j<=sum;j++)
+                            {
+                                    if(nums[i-1]<=j)
+                                    {
+                                            t[i][j] = t[i-1][j-nums[i-1]]||t[i-1][j];
+                                    }
+                                    else
+                                            t[i][j]=t[i-1][j];
+                            }
+                    }
+            
+            return t[n][sum];
     }
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (nums[i - 1] <= j) {
-                t[i][j] = t[i - 1][j - nums[i - 1]] || t[i - 1][j];
-            } else {
-                t[i][j] = t[i - 1][j];
-            }
-        }
-    }
-
-    return t[n][sum];
-}
     bool canPartition(vector<int>& nums) {
             int n=nums.size();
             int sum=0;
